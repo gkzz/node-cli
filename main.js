0ea716885f28c9f4b6ef0a54c4f137b35c923458
gkzz <gakuji-tamaki@cybozu.co.jp>
2022-07-28 15:37:23 +0900

    「ユニットテストを記述する」をやった

--- main.js
+++ main.js
@@ -1,33 +1,23 @@
 const program = require("commander");
 const fs = require("fs");
-const marked = require("marked");
+const md2html = require("./md2html");
 
-program.option("--gfm", "GFM Enabled");
+program.option("--gfm", "GFMを有効にする");
 program.parse(process.argv);
 const filePath = program.args[0];
 
-const opt = program.opts();
-
 const cliOptions = {
-    // opt.gfm に false を代入するかどうかなので、`??=` を使うべき
-    // https://jsprimer.net/use-case/nodecli/md-to-html/
-    //gfm: opt.gfm ?? false
-    gfm: opt.gfm ??= false
-}
+    gfm: false,
+    ...program.opts(),
+};
 
-/*
- * ファイルを非同期で読み込む
- * fsモジュールのコールバック関数の第一引数には常にエラーオブジェクト
- */
 fs.readFile(filePath, { encoding: "utf8" }, (err, file) => {
     if (err) {
-        console.error(err.message);
-        // 終了ステータス 1（一般的なエラー）としてプロセスを終了する
+        console.error(err);
         process.exit(1);
         return;
     }
-    const html = marked.parse(file, {
-        gfm: cliOptions.gfm,
-    });
+    // md2htmlモジュールを使ってHTMLに変換する
+    const html = md2html(file, cliOptions);
     console.log(html);
 });
